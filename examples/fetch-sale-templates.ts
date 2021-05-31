261803cf7dd518b151ca9abec928d443859a5dcb
Adam Azad <adam@adamazad.com>
2021-05-31 12:51:03 +0200

    Clean up

--- examples/fetch-sale-templates.ts
+++ examples/fetch-sale-templates.ts
@@ -1,11 +1,8 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { Wallet } from 'ethers'
-import dayjs from 'dayjs'
 // Import lib
 import { Mesa, XDAI_CONFIG } from '../src'
 import { mnemonic } from '../secrets.json'
-
-dayjs.extend(require('dayjs/plugin/utc'))
 ;(async () => {
   const provider = new JsonRpcProvider('https://rpc.xdaichain.com/')
   const wallet = Wallet.fromMnemonic(mnemonic)
