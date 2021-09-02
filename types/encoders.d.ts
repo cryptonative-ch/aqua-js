6bb178e81d68bbf3ef49a5a5711fc8ae3079f9ef
Daniel Cybulka <danielcybulka@gmail.com>
2021-09-02 15:40:17 +0200

    remove types from repo

--- types/encoders.d.ts
+++
@@ -1,3 +0,0 @@
-import { InitDataFairSaleOptions, InitDataFixedPriceSaleOptions } from './types/encoders';
-export declare function encodeInitDataFairSale({ saleLauncher, saleTemplateId, tokenOut, tokenIn, auctionEndDate, tokenOutSupply, minPrice, minBuyAmount, minRaise, tokenSupplier, }: InitDataFairSaleOptions): string;
-export declare function encodeInitDataFixedPriceSale({ saleLauncher, saleTemplateId, tokenSupplier, tokenIn, tokenOut, tokenPrice, tokensForSale, startDate, endDate, minCommitment, maxCommitment, minRaise, participantList, }: InitDataFixedPriceSaleOptions): string;
--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { AquaConfigMap } from './types';
-export * from './Aqua';
-export * from './Subgraph';
-export * from './types';
-export * as encoders from './encoders';
-export * from './contracts';
-export declare const XDAI_CONFIG: AquaConfigMap;
-export declare const RINKEBY_CONFIG: AquaConfigMap;
